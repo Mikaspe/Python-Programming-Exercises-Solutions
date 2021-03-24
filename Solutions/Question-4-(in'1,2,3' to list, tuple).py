68f719ff51d553fe5fa7d4121936760863ea31dd
Milosz Kasper <milosz.kasper@gmail.com>
2021-03-24 14:35:55 +0100

    Question-4 solution

---
+++ Solutions/Question-4-(in'1,2,3' to list, tuple).py
@@ -0,0 +1,8 @@
+NumInList = input('Pick a number:').split(',')
+
+print(NumInList, tuple(NumInList))
+
+for i in range(len(NumInList)):  # Conversion str to int
+    NumInList[i] = int(NumInList[i])
+
+print(NumInList, tuple(NumInList))
