af88c8feb219c8b68a4790e5e0711d6983feb41c
Milosz Kasper <milosz.kasper@gmail.com>
2021-04-08 13:07:40 +0200

    Completed 'Question 5' and gitignore update

--- Solutions/Question-5.py
+++ Solutions/Question-5.py
@@ -1,3 +1,23 @@
-class MyClass():
-    def getString():
-        input()
+class MyClass:
+    """This is class for string operations"""
+
+    def __init__(self):
+        self.str = 'First use .getString method'
+
+    def getString(self):
+        self.str = input('Type a string: ')
+
+    def printString(self):
+        print(self.str.upper())
+
+
+def TestForClass(ClassToTest):
+    test = ClassToTest()
+    print(test.__doc__)
+    test.getString()
+    test.printString()
+
+
+TestForClass(MyClass)
+
+
