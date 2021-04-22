83c6c75f8d6fbcbbb90672769917832e6fcbec71
Mikaspe <milosz.kasper@gmail.com>
2021-04-22 12:15:13 +0200

    Some syntax fixes

--- Solutions(91-100)/Question-97-(AlphabetRangoli).py
+++ Solutions(91-100)/Question-97-(AlphabetRangoli).py
@@ -1,20 +1,20 @@
-#My own solution
+#My first solution
 from string import ascii_lowercase
 N = 25
 
 y_size = 4*N-3
 
-y_mid = int((4*N-3)/2) # Middle column
+y_mid = int((4*N-3)/2)  # Middle column
 alphabet = ascii_lowercase[N-1::-1]
 
 rangoli = [['-' for y in range(y_size)] for x in range(N)]
-index_letter = [] # Index in which column will be a letter
+index_letter = []  # Index in which column will be a letter
 
 for x in range(N):
     index_letter[:0] = [y_mid - 2 * x, y_mid + 2 * x]
     print(index_letter)
     for k in range(N):
-        for y in index_letter[2*k:2*k+2]:
+        for y in index_letter[2*k : 2*k+2]:
             print(y)
             rangoli[x][y] = alphabet[k]
 
